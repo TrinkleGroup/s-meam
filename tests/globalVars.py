14c28615d1ba7c74e1a7d47a8c8078fa7de83433
Josh Vita <vita.joshua@gmail.com>
2017-10-19 15:33:11 -0500

    Testing commit behavior

--- tests/globalVars.py
+++ tests/globalVars.py
@@ -1,7 +1,8 @@
-import numpy as np
+"""Global testing parameters"""
 
-a0 = 2.6
-vac = 2*a0
+a0 = 2.6    # Potential cutoff distance
+r0 = a0/4   # Default atomic spacing
+vac = 2*a0  # Vacuum size used in all directions for certain structures
 
-DIGITS = 6
-ATOL = 1e-5
+DIGITS = 6  # Desired number of digits for test accuracy (assertAlmostEquals)
+ATOL = 1e-5 # Absolute tolerance for test accuracy (np.assert_allclose)
