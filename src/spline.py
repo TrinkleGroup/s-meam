cdcafe3c8136d701ca7b1cdeaa816f3f31ace438
Josh Vita <vita.joshua@gmail.com>
2018-01-09 09:15:24 -0600

    1-9: streamlined Spline and improved testing

--- src/spline.py
+++ src/spline.py
@@ -7,39 +7,29 @@ import numpy as np
 
 class Spline(CubicSpline):
 
-    def __init__(self,x,y,bc_type='natural', derivs=(0,0)):
+    def __init__(self, x=[], y=[], end_derivs=None):
+        """Assumes that boundary conditions are either 'natural' on both
+        sides, or fixed with given 1st derivatives"""
 
-        super(Spline,self).__init__(x,y,bc_type=bc_type)#,bc_type=((1,d0),(1,dN)))
+        if end_derivs is None:
+            bc = ('natural', 'natural')
+        elif len(end_derivs) == 2:
+            bc = ((1,end_derivs[0]), (1,end_derivs[1]))
+        else:
+            raise ValueError("Must specify exactly 2 end derivatives OR leave"
+                             "blank for natural boundary conditions")
+
+        super(Spline,self).__init__(x,y,bc_type=bc, extrapolate=True)
+
+        self.d0 = self(x[0], 1)
+        self.dN = self(x[-1], 1)
         self.cutoff = (x[0],x[len(x)-1])
-        self.bc_type = bc_type
-
-        self.d0, self.dN = derivs
         self.h = x[1]-x[0]
-        self.knotsx = x
-        self.knotsy = y
-
-        self.knotsy1 = np.array([super(Spline,self).__call__(z,1) for z in x])
-        self.knotsy2 = np.array([super(Spline,self).__call__(z,2) for z in x])
-
-        # Addcoefficients for extrapolating splines; can't edit self.c itself
-        self.cmat = np.copy(self.c)
-        self.cmat = np.insert(self.c,0,np.array([0,0,self.d0,self.knotsy[
-            0]]),axis=1)
-        self.cmat = np.insert(self.cmat,self.cmat.shape[1],np.array([0,0,\
-                        self.dN, self.knotsy[-1]]),axis=1)
 
     def in_range(self, x):
         """Checks if a given value is within the spline's cutoff range"""
 
         return (x >= self.cutoff[0]) and (x <= self.cutoff[1])
-
-    def extrap(self, x):
-        """Performs linear extrapolation past the endpoints of the spline"""
-
-        if x < self.cutoff[0]:
-            return self(self.x[0]) - self.d0*(self.x[0]-x)
-        elif x > self.cutoff[1]:
-            return self(self.x[-1]) + self.dN*(x-self.x[-1])
 
     def plot(self,xr=None,yr=None,xl=None,yl=None,saveName=None):
         """Plots the spline"""
@@ -64,35 +54,14 @@ class Spline(CubicSpline):
         if saveName: plt.savefig(saveName)
         else: plt.show()
 
-    def __call__(self,x,i=None):
-        """Evaluates the spline at the given point, linearly extrapolating if
-        outside of the spline cutoff. If 'i' is specified, evaluates the ith
-        derivative instead."""
-
-        if i:
-            if x <= self.cutoff[0]:
-                return self.d0
-            elif x >= self.cutoff[1]:
-                return self.dN
-            else:
-                return super(Spline,self).__call__(x,i)
-
-        if type(x) == np.ndarray: # use CubicSpline.__call__()
-            return super(Spline, self).__call__(x)
-        else:
-            if self.in_range(x):
-                return super(Spline,self).__call__(x)
-            else:
-                return self.extrap(x)
-
     # TODO: add a to_matrix() function for matrix form?
 
 class ZeroSpline(Spline):
     """Used to easily create a zero spline"""
 
-    def __init__(self,knotsx):
+    def __init__(self, knotsx):
 
         knotsx = np.array(knotsx)
 
-        super(ZeroSpline,self).__init__(knotsx,np.zeros(knotsx.shape[0]),\
-                derivs=(0,0))
+        super(ZeroSpline, self).__init__(knotsx, np.zeros(knotsx.shape[0]),\
+                derivs=(0.,0.))
--- tests/splineTests.py
+++ tests/splineTests.py
@@ -1,27 +1,141 @@
 import unittest
 import numpy as np
+
 from spline import Spline
 
-class SplineTests(unittest.TestCase):
+class ConstructorTests(unittest.TestCase):
+    """Verifies appropriate constructor behavior"""
+
+    # Note: mosterror handling goes through scipy.CubicSpline
+    def test_missing_data(self):
+        self.assertRaises(ValueError, Spline)
+
+    def test_too_many_derivs(self):
+        x = np.arange(10, dtype=float)
+        y = np.arange(10, dtype=float)
+
+        self.assertRaises(ValueError, Spline, x, y, (1,2,3))
+
+    def test_uneven_xy(self):
+        x = np.arange(10)
+        y = np.arange(9)
+
+        self.assertRaises(ValueError, Spline, x, y)
+
+    def test_overlapping_knots(self):
+        x = np.array([0,1,1])
+        y = np.array([0,2,2])
+
+        self.assertRaises(ValueError, Spline, x, y)
+
+    def test_basic_data(self):
+        x = np.arange(10, dtype=float)
+        y = np.arange(10, dtype=float)
+
+        s = Spline(x, y)
+
+        np.testing.assert_allclose(s.x, x)
+        np.testing.assert_allclose(s(s.x), y)
+        self.assertEquals(s.cutoff, (0., 9.))
+        self.assertEquals(s.d0, 1.0)
+        self.assertEquals(s.dN, 1.0)
+        self.assertAlmostEqual(s.h, 1.0, 15)
+
+    def test_natural_bc(self):
+        d0_expected = 0.0077693494604539
+        dN_expected = 0.105197706160344
+        y2_expected = np.array([0.00152870603877451, 0.00038933722543571,
+                       0.00038124926922248, 0.0156639264890892])
+
+        x = np.array([-55.1423316, -44.7409899033333, -34.3396481566667,
+                  -23.9383064])
+        y = np.array([-0.2974556800, -0.15449458722, 0.05098657168,
+                     0.57342694704])
+
+        s = Spline(x, y, (d0_expected, dN_expected))
+
+        self.assertAlmostEqual(s.d0, d0_expected)
+        self.assertAlmostEqual(s.dN, dN_expected)
+        np.testing.assert_allclose(s(s.x, 2), y2_expected)
+
+    def test_one_set_derivative(self):
+        d0_expected = 0.15500135578733
+        dN_expected = 0.
+        y2_expected = np.array([0, -1.60311717015859, 1.19940299483249,
+                                1.47909794595154, -2.49521499855605])
+
+        x = np.array([1.9, 2.8, 3.7, 4.6, 5.5])
+        y = np.array([0.533321679606674, 0.456402081843862, -0.324281383502201,
+                      -0.474029826906675, 0])
+
+        s = Spline(x, y, end_derivs=(d0_expected, dN_expected))
+
+        self.assertAlmostEqual(s.d0, d0_expected)
+        self.assertAlmostEqual(s.dN, dN_expected)
+        np.testing.assert_allclose(s(s.x, 2), y2_expected, atol=1e-15)
+
+# rzm: test evaluations; internal, @ knots, EXTRAPOLATION
+class EvaluationTests(unittest.TestCase):
     """Test cases for Spline class"""
 
-    def test_setters(self):
-        xi = np.arange(1,10)
-        yi = np.sin(xi)
+    def test_eval_knot_single(self):
+        x = np.arange(10, dtype=float)
+        y = np.arange(10, dtype=float)
 
-        s = Spline(xi,yi)
-        
-        s.d0 = 100
-        self.assertEquals(s.d0, 100)
+        d0 = dN = 1.
 
-        s.dN = 7
-        self.assertEquals(s.dN, 7)
+        s = Spline(x, y, end_derivs=(d0,dN))
 
-    def test_basic(self):
+        self.assertAlmostEqual(s(x[4]), 4.)
+
+    def test_eval_knot_all(self):
+        x = np.arange(10, dtype=float)
+        y = np.arange(10, dtype=float)
+
+        d0 = dN = 1.
+
+        s = Spline(x, y, end_derivs=(d0,dN))
+
+        np.testing.assert_allclose(s(x), y)
+
+    def test_eval_extrap_single_LHS(self):
+        x = np.arange(10, dtype=float)
+        y = np.arange(10, dtype=float)
+
+        d0 = dN = 1.
+
+        s = Spline(x, y, end_derivs=(d0,dN))
+
+        np.testing.assert_allclose(s(-1), -1.)
+
+    def test_eval_extrap_single_RHS(self):
+        x = np.arange(10, dtype=float)
+        y = np.arange(10, dtype=float)
+
+        d0 = dN = 1.
+
+        s = Spline(x, y, end_derivs=(d0,dN))
+
+        np.testing.assert_allclose(s(11), 11.)
+
+    def test_eval_extrap_many(self):
+        x = np.arange(10, dtype=float)
+        y = np.arange(10, dtype=float)
+
+        d0 = dN = 1.
+
+        s = Spline(x, y, end_derivs=(d0,dN))
+
+        x = np.concatenate(([-1], y, [11]))
+        y = np.concatenate(([-1], y, [11]))
+
+        np.testing.assert_allclose(s(x), y)
+
+    def test_sin_fxn(self):
         xi = np.linspace(0,2*np.pi,10)
         yi = np.sin(xi)
 
-        s = Spline(xi,yi)
+        s = Spline(xi, yi)
 
         # Check knot values
         for i in range(len(xi)):
@@ -33,6 +147,18 @@ class SplineTests(unittest.TestCase):
         self.assertAlmostEqual(s(np.pi), 0.0, places=3)
         self.assertAlmostEqual(s(3*np.pi/2.), -1.0, places=3)
         self.assertAlmostEqual(s(2*np.pi), 0.0, places=3)
+
+class MethodTests(unittest.TestCase):
+
+    def test_in_range(self):
+        x = np.arange(10, dtype=float)
+        y = np.arange(10, dtype=float)
+
+        s = Spline(x, y)
+
+        self.assertTrue(s.in_range(4))
+        self.assertFalse(s.in_range(-1))
+        self.assertFalse(s.in_range(11))
 
 if __name__ == "__main__":
     unittest.main()
