1-17: added force calculations to meam.py; corrected ASE precision loss issue

src/meam.py
@@ -312,7 +312,159 @@ class MEAM:
 
         return total_pe
 
-    def compute_lammps_results(self, struct):
+    def compute_forces(self, atoms):
+        """Evaluates the energies for the given system using the MEAM potential,
+        following the notation specified in the LAMMPS spline/meam documentation
+        Args:
+            atoms (Atoms):
+                ASE Atoms object containing all atomic information
+
+        Note that splines are indexed as follows:
+            phis, gs: 11,12,...,1N,22,...,2N,...,NN
+            rhos, us, fs: 1,2,...,N
+
+            e.g. for Ti-O where Ti is atom type 1, O atom type 2
+                phis, gs: Ti-Ti, Ti-O, O-O
+                rhos, us, fs: Ti, O"""
+
+        self.compute_energy(atoms)
+
+        nl = NeighborList(np.ones(len(atoms)) * (self.cutoff / 2), \
+                          self_interaction=False, bothways=True, skin=0.0)
+        nl_noboth = NeighborList(np.ones(len(atoms)) * (self.cutoff / 2), \
+                                 self_interaction=False, bothways=False,
+                                 skin=0.0)
+        nl.build(atoms)
+        nl_noboth.build(atoms)
+
+        natoms = len(atoms)
+
+        self.forces = np.zeros((natoms, 3))
+        cellx, celly, cellz = atoms.get_cell()
+
+        for i in range(natoms):
+            itype = lammpsTools.symbol_to_type(atoms[i].symbol, self.types)
+            ipos = atoms[i].position
+
+            Uprime_i = self.uprimes[i]
+
+            # Pull atom-specific neighbor lists
+            neighbors = nl.get_neighbors(i)
+            neighbors_noboth = nl_noboth.get_neighbors(i)
+
+            num_neighbors = len(neighbors[0])
+            num_neighbors_noboth = len(neighbors_noboth[0])
+
+            # Build the list of shifted positions for atoms outside of unit cell
+            neighbor_shifted_positions = []
+            for l in range(num_neighbors):
+                shiftx, shifty, shiftz = neighbors[1][l]
+                neigh_pos = atoms[neighbors[0][
+                    l]].position + shiftx * cellx + shifty * celly + \
+                            shiftz * cellz
+
+                neighbor_shifted_positions.append(neigh_pos)
+            # end shifted positions loop
+
+            forces_i = np.zeros((3,))
+            if len(neighbors[0]) > 0:
+                for j in range(num_neighbors):
+                    jtype = lammpsTools.symbol_to_type(
+                        atoms[neighbors[0][j]].symbol, self.types)
+
+                    jpos = neighbor_shifted_positions[j]
+                    jdel = jpos - ipos
+                    r_ij = np.linalg.norm(jdel)
+                    jdel /= r_ij
+
+                    fj_val = self.fs[i_to_potl(jtype)](r_ij)
+                    fj_prime = self.fs[i_to_potl(jtype)](r_ij, 1)
+
+                    forces_j = np.zeros((3,))
+
+                    # Used for triplet calculations
+                    a = neighbor_shifted_positions[j] - ipos
+                    na = np.linalg.norm(a)
+
+                    for k in range(j, num_neighbors):
+                        if k != j:
+                            ktype = lammpsTools.symbol_to_type( \
+                                atoms[neighbors[0][k]].symbol, self.types)
+                            kpos = neighbor_shifted_positions[k]
+                            kdel = kpos - ipos
+                            r_ik = np.linalg.norm(kdel)
+                            kdel /= r_ik
+
+                            # TODO: b == kdel
+                            b = neighbor_shifted_positions[k] - ipos
+                            nb = np.linalg.norm(b)
+
+                            # TODO: try get_dihedral() for angles
+                            cos_theta = np.dot(a, b) / na / nb
+
+                            fk_val = self.fs[i_to_potl(ktype)](r_ik)
+                            g_val = self.gs[
+                                ij_to_potl(jtype, ktype, self.ntypes) \
+                                ](cos_theta)
+
+                            fk_prime = self.fs[i_to_potl(ktype)](r_ik, 1)
+                            g_prime = self.gs[ij_to_potl(jtype, ktype, \
+                                                     self.ntypes)](cos_theta, 1)
+
+                            fij = -Uprime_i * g_val * fk_val * fj_prime
+                            fik = -Uprime_i * g_val * fj_val * fk_prime
+
+                            prefactor = Uprime_i * fj_val * fk_val * g_prime
+                            prefactor_ij = prefactor / r_ij
+                            prefactor_ik = prefactor / r_ik
+                            fij += prefactor_ij * cos_theta
+                            fik += prefactor_ik * cos_theta
+
+                            fj = jdel * fij - kdel * prefactor_ij
+                            forces_j += fj
+
+                            fk = kdel * fik - jdel * prefactor_ik
+                            forces_i -= fk
+
+                            self.forces[neighbors[0][k]] += fk
+                    # end triplet loop
+
+                    self.forces[i] -= forces_j
+                    self.forces[neighbors[0][j]] += forces_j
+                # end pair loop
+
+                self.forces[i] += forces_i
+
+                # Calculate pair interactions (phi)
+                for j in range(
+                        num_neighbors_noboth):  # j = index for neighbor list
+                    jtype = lammpsTools.symbol_to_type( \
+                        atoms[neighbors_noboth[0][j]].symbol, self.types)
+                    jpos = neighbor_shifted_positions[j]
+                    jdel = jpos - ipos
+                    r_ij = np.linalg.norm(jdel)
+
+                    rho_prime_i = self.rhos[i_to_potl(itype)](r_ij, 1)
+                    rho_prime_j = self.rhos[i_to_potl(jtype)](r_ij, 1)
+
+                    fpair = rho_prime_j * self.uprimes[i] + \
+                            rho_prime_i * self.uprimes[neighbors_noboth[0][j]]
+
+                    phi_prime = self.phis[ij_to_potl(itype, jtype, self.ntypes)] \
+                        (r_ij, 1)
+
+                    fpair += phi_prime
+                    fpair /= r_ij
+
+                    self.forces[i] += jdel * fpair
+                    self.forces[neighbors_noboth[0][j]] -= jdel * fpair
+                # end phi loop
+
+            # end atom loop
+
+        return self.forces
+
+    def get_lammps_results(self, struct):
 
         types = ['H','He']
 
@@ -326,8 +478,9 @@ class MEAM:
 
         self.write_to_file('test.meam.spline')
 
-        calc = LAMMPS(no_data_file=True, parameters=params, \
-                      keep_tmp_files=False,specorder=types,files=['test.meam.spline'])
+        calc = LAMMPS(no_data_file=True, parameters=params,
+                      keep_tmp_files=False, specorder=types,
+                      files=['test.meam.spline'])
 
         energy = calc.get_potential_energy(struct)
         forces = calc.get_forces(struct)

src/worker.py
@@ -320,7 +320,10 @@ class Worker:
 
         return energy - zero_point_energy
 
-    # def compute_forces(self, potentials):
+    def compute_forces(self, potentials):
+        forces = np.zeros((len(self.atoms), 3))
+
+        return forces
     #     """Calculates energies for all potentials using information
     #     pre-computed during initialization."""
     #     #TODO: only thing changing should be y-coords?

tests/TestMeam.py
@@ -10,7 +10,7 @@ from spline import Spline, ZeroSpline
 from tests.testStructs import dimers, trimers, bulk_periodic_ortho,\
     bulk_vac_ortho, bulk_periodic_rhombo, bulk_vac_rhombo
 
-DIGITS = 6 # LAMMPS results are only accurate to 1e-6 b/c of read/write
+DIGITS = 15
 EPS = 1e-15
 
 class ConstructorTests(unittest.TestCase):
@@ -159,9 +159,9 @@ class MethodTests(unittest.TestCase):
 
         new_x_pvec, new_y_pvec, new_x_indices = meam.splines_to_pvec(splines)
 
-        np.testing.assert_allclose(new_x_pvec, x_pvec, atol=EPS)
-        np.testing.assert_allclose(new_y_pvec, y_pvec, atol=EPS)
-        np.testing.assert_allclose(new_x_indices, x_indices, atol=EPS)
+        np.testing.assert_allclose(new_x_pvec, x_pvec, atol=1e-15)
+        np.testing.assert_allclose(new_y_pvec, y_pvec, atol=1e-15)
+        np.testing.assert_allclose(new_x_indices, x_indices, atol=1e-15)
 
     def test_splines_to_vec_file(self):
         p = MEAM.from_file('../data/pot_files/TiO.meam.spline')
@@ -354,13 +354,10 @@ class MethodTests(unittest.TestCase):
             else:
                 self.assertTrue(isinstance(all_splines[i], ZeroSpline))
 
-class EvaluationTests(unittest.TestCase):
+class EnergyTests(unittest.TestCase):
 
     def setUp(self):
         self.p = MEAM.from_file("../data/pot_files/HHe.meam.spline")
-
-    def test_zero_atom_energies(self):
-        pass
 
     def test_energy_dimers(self):
 
@@ -368,7 +365,7 @@ class EvaluationTests(unittest.TestCase):
             atoms = dimers[name]
 
             guess = self.p.compute_energy(atoms)
-            true = self.p.compute_lammps_results(atoms)['energy']
+            true = self.p.get_lammps_results(atoms)['energy']
 
         self.assertAlmostEqual(guess, true, places=DIGITS)
 
@@ -378,7 +375,7 @@ class EvaluationTests(unittest.TestCase):
             atoms = trimers[name]
 
             guess = self.p.compute_energy(atoms)
-            true = self.p.compute_lammps_results(atoms)['energy']
+            true = self.p.get_lammps_results(atoms)['energy']
 
         self.assertAlmostEqual(guess, true, places=DIGITS)
 
@@ -388,7 +385,7 @@ class EvaluationTests(unittest.TestCase):
             atoms = bulk_vac_rhombo[name]
 
             guess = self.p.compute_energy(atoms)
-            true = self.p.compute_lammps_results(atoms)['energy']
+            true = self.p.get_lammps_results(atoms)['energy']
 
         self.assertAlmostEqual(guess, true, places=DIGITS)
 
@@ -398,7 +395,7 @@ class EvaluationTests(unittest.TestCase):
             atoms = bulk_periodic_rhombo[name]
 
             guess = self.p.compute_energy(atoms)
-            true = self.p.compute_lammps_results(atoms)['energy']
+            true = self.p.get_lammps_results(atoms)['energy']
 
         self.assertAlmostEqual(guess, true, places=DIGITS)
 
@@ -408,7 +405,7 @@ class EvaluationTests(unittest.TestCase):
             atoms = bulk_periodic_ortho[name]
 
             guess = self.p.compute_energy(atoms)
-            true = self.p.compute_lammps_results(atoms)['energy']
+            true = self.p.get_lammps_results(atoms)['energy']
 
         self.assertAlmostEqual(guess, true, places=DIGITS)
 
@@ -418,24 +415,74 @@ class EvaluationTests(unittest.TestCase):
             atoms = bulk_vac_ortho[name]
 
             guess = self.p.compute_energy(atoms)
-            true = self.p.compute_lammps_results(atoms)['energy']
+            true = self.p.get_lammps_results(atoms)['energy']
 
         self.assertAlmostEqual(guess, true, places=DIGITS)
 
+class ForcesTests(unittest.TestCase):
+
+    def setUp(self):
+        self.p = MEAM.from_file("../data/pot_files/HHe.meam.spline")
+
     def test_forces_dimer(self):
-        pass
+
+        tmp_dimers = {'aa':dimers['aa']}
+
+        for name in tmp_dimers.keys():
+            atoms = tmp_dimers[name]
+
+            guess = self.p.compute_forces(atoms)
+            true = self.p.get_lammps_results(atoms)['forces']
+
+            np.testing.assert_allclose(guess, true, atol=EPS)
 
     def test_forces_trimer(self):
-        pass
+
+        for name in trimers.keys():
+            atoms = trimers[name]
+
+            guess = self.p.compute_forces(atoms)
+            true = self.p.get_lammps_results(atoms)['forces']
+
+            np.testing.assert_allclose(guess, true, atol=EPS)
 
     def test_forces_bulk_vac_ortho(self):
-        pass
+
+        for name in bulk_vac_ortho.keys():
+            atoms = bulk_vac_ortho[name]
+
+            guess = self.p.compute_forces(atoms)
+            true = self.p.get_lammps_results(atoms)['forces']
+
+            np.testing.assert_allclose(guess, true, atol=EPS)
 
     def test_forces_bulk_vac_rhombo(self):
-        pass
+
+        for name in bulk_vac_rhombo.keys():
+            atoms = bulk_vac_rhombo[name]
+
+            guess = self.p.compute_forces(atoms)
+            true = self.p.get_lammps_results(atoms)['forces']
+
+            np.testing.assert_allclose(guess, true, atol=EPS)
 
     def test_forces_bulk_periodic_ortho(self):
-        pass
+
+        for name in bulk_periodic_ortho.keys():
+            atoms = bulk_periodic_ortho[name]
+
+            guess = self.p.compute_forces(atoms)
+            true = self.p.get_lammps_results(atoms)['forces']
+
+            np.testing.assert_allclose(guess, true, atol=EPS)
 
     def test_forces_bulk_periodic_rhombo(self):
-        pass
+
+        for name in bulk_periodic_rhombo.keys():
+            atoms = bulk_periodic_rhombo[name]
+
+            guess = self.p.compute_forces(atoms)
+            true = self.p.get_lammps_results(atoms)['forces']
+
+            np.testing.assert_allclose(guess, true, atol=EPS)
+

tests/TestWorker.py
@@ -24,15 +24,15 @@ np.random.seed(42)
 N = 1
 
 # Flags for what tests to run
-energy_flag = True*1
-forces_flag = True*0
+energy_flag = True*0
+forces_flag = True*1
 
-zero_pots_flag  = True*0
-const_pots_flag = True*0
-rand_pots_flag  = True*1
+zero_pots_flag  = True*1
+const_pots_flag = True*1
+rand_pots_flag  = True*0
 
-meam_flag       = True*1
-phionly_flag    = True*0
+meam_flag       = True*0
+phionly_flag    = True*1
 rhophi_flag     = True*0
 nophi_flag      = True*0
 rho_flag        = True*0
@@ -41,7 +41,7 @@ norhophi_flag   = True*0
 
 dimers_flag  = True*1
 trimers_flag = True*1
-bulk_flag    = True*1
+bulk_flag    = True*0
 
 allstructs = {}
 
@@ -51,7 +51,7 @@ if trimers_flag:
     allstructs = {**allstructs, **trimers}
 if bulk_flag:
     allstructs = {**allstructs, **bulk_vac_ortho, **bulk_periodic_ortho,
-                  **bulk_vac_rhombo, **bulk_periodic_rhombo}
+                  **bulk_vac_rhombo, **bulk_periodic_rhombo, **extra}
 
 # allstructs = {'aba':trimers['aba']}
 
@@ -103,7 +103,7 @@ def getLammpsResults(pots, structs):
             val = p.compute_energy(atoms)
 
             # cstart = time.time()
-            results = p.compute_lammps_results(atoms)
+            results = p.get_lammps_results(atoms)
             energies[name][pnum] = results['energy']
             forces[name].append(results['forces'])
             # lammps_calcduration += float(time.time() - cstart)
@@ -145,7 +145,7 @@ def runner_forces(pots, structs):
         start = time.time()
         # TODO: to optimize, preserve workers for each struct
         forces[name] = w.compute_forces(pots[0])
-        py_calcduration += time.time() - start
+        # py_calcduration += time.time() - start
 
     return forces
 

tests/testStructs.py
@@ -160,16 +160,16 @@ bulk_vac_rhombo['bulk_vac_rhombo_mixed'] = mixed
 
 ################################################################################
 
-extra = ase.build.bulk('H', crystalstructure='fcc', a=a0, orthorhombic=True)
-extra = extra.repeat((2,1,1))
-extra.rattle()
-extra.center(vacuum=0)
-extra.set_pbc(True)
-extra.set_chemical_symbols(np.random.randint(1,3, size=len(extra)))
+atoms4 = ase.build.bulk('H', crystalstructure='fcc', a=a0, orthorhombic=True)
+atoms4 = atoms4.repeat((2,1,1))
+atoms4.rattle()
+atoms4.center(vacuum=0)
+atoms4.set_pbc(True)
+atoms4.set_chemical_symbols(np.random.randint(1,3, size=len(atoms4)))
 
-extra.center(vacuum=vac)
+atoms4.center(vacuum=vac)
 
-extra = {'extra':extra}
+extra = {'4_atoms':atoms4}
 
 ################################################################################
 allstructs = {**dimers, **trimers, **bulk_vac_ortho, **bulk_periodic_ortho,\