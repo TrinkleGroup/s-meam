1-25: correct energies; fixed extrapolation and further ASE precision issues

src/lammpsTools.py
@@ -65,7 +65,7 @@ def read_spline_meam(fname):
 
             d0, dN = [float(el) for el in f.readline().split()]
             all_d0.append(d0); all_dN.append(dN)
-            # rzm: do we need to keep d0, dN? also, move this all to worker.py
+            # TODO: do we need to keep d0, dN? also, move this all to worker.py
 
             for j in range(nknots):
                 x,y,y2 = [np.float(el) for el in f.readline().split()]
@@ -288,7 +288,6 @@ def atoms_to_LAMMPS_file(fname, atoms):
     p = atoms.positions
 
     # Converts cell vectors to LAMMPS format
-    print(atoms.get_cell())
     a,b,c = atoms.get_cell()
     x = a[0]; y = b[1]; z = c[2]
     xy = b[0]; xz = c[0]; yz = c[1]
@@ -308,7 +307,7 @@ def atoms_to_LAMMPS_file(fname, atoms):
 
         for i in range(len(types_idx)):
             f.write("%d %d %.16f %.16f %.16f\n" % (i+1,\
-                symbol_to_type(types[i],types),p[i][0],p[i][1],p[i][2]))
+                symbol_to_type(types[i-1],types),p[i][0],p[i][1],p[i][2]))
 
 def read_forces(fname):
     """Reads in the atomic forces from a file with the following format:

src/meam.py
@@ -245,6 +245,7 @@ class MEAM:
                         ipos - neighbor_shifted_positions[j])
 
                     phi = self.phis[ij_to_potl(itype, jtype, self.ntypes)]
+                    # print("MEAM: phi({0}) = {1}".format(r_ij, phi(r_ij)))
 
                     total_phi += phi(r_ij)
                 # end phi loop
@@ -282,6 +283,7 @@ class MEAM:
                             #                                    b, na,nb))
                             # TODO: try get_dihedral() for angles
                             cos_theta = np.dot(a, b) / na / nb
+                            # logging.info("MEAM: cos_theta = %.16f" % cos_theta)
 
                             # logging.info("MEAM: {0}, {1}, {2}".format(r_ij,
                             #                                 r_ik, cos_theta))
@@ -296,7 +298,9 @@ class MEAM:
                     fj_val = fj(r_ij)
                     total_ni += fj_val * partialsum
                     total_ni += rho(r_ij)
-                    # logging.info("MEAM rho({0} = {1}: ".format(r_ij, rho(r_ij)))
+                    print("MEAM: rho({0}) = {1}".format(r_ij, rho(r_ij)))
+                    #logging.info("MEAM: j = {2}, rho({0}) = {1}".format(r_ij,
+                                                            #rho(r_ij), jtype))
                     # logging.info("MEAM ffg() = {0}: ".format(fj_val*partialsum))
                 # end u loop
 
@@ -308,6 +312,9 @@ class MEAM:
                 total_pe += atom_e
 
                 self.uprimes[i] = u(total_ni, 1)
+                # logging.info("MEAM: U'({0}) = {1}".format(total_ni,
+                #                                           self.uprimes[i]))
+                # logging.info("MEAM: U({0}) = {1}".format(total_ni,u(total_ni)))
             # end atom loop
 
         return total_pe
@@ -395,7 +402,6 @@ class MEAM:
                             r_ik = np.linalg.norm(kdel)
                             kdel /= r_ik
 
-                            # TODO: b == kdel
                             b = neighbor_shifted_positions[k] - ipos
                             nb = np.linalg.norm(b)
 

src/worker.py
@@ -13,8 +13,6 @@ from workerSplines import WorkerSpline, RhoSpline, ffgSpline
 logger = logging.getLogger(__name__)
 # logging.basicConfig(filename='worker.log')
 
-# rzm: begin force evaluations
-
 class Worker:
     """Worker designed to compute energies and forces for a single structure
     using a variety of different MEAM potentials
@@ -70,7 +68,9 @@ class Worker:
         self.ntypes     = ntypes
 
         nphi            = int((self.ntypes+1)*self.ntypes/2)
-        self.nphi       = nphi
+        self.nphi       = nphi # there are nphi phi functions and nphi g fxns
+
+        self.uprimes    = np.zeros(len(atoms))
 
         # Initialize splines; group by type and calculate potential cutoff range
 
@@ -115,6 +115,7 @@ class Worker:
 
         self.ffgs = []
 
+        # Build all combinations of ffg splines
         for j in range(len(self.fs)):
             inner_list = []
 
@@ -126,14 +127,8 @@ class Worker:
                 inner_list.append(ffgSpline(fj, fk, g, len(self.atoms)))
             self.ffgs.append(inner_list)
 
-        self.max_phi_knots  = max([len(s.x) for s in self.phis])
-        self.max_rho_knots  = max([len(s.x) for s in self.rhos])
-        self.max_u_knots    = max([len(s.x) for s in self.us])
-        self.max_f_knots    = max([len(s.x) for s in self.fs])
-        self.max_g_knots    = max([len(s.x) for s in self.gs])
-
+        # Compute full potential cutoff distance (based only on radial fxns)
         radial_fxns = self.phis + self.rhos + self.fs
-
         self.cutoff = np.max([max(s.x) for s in radial_fxns])
 
         # Building neighbor lists
@@ -177,6 +172,7 @@ class Worker:
 
                 self.phis[phi_idx].add_to_struct_vec(rij)
 
+
             # Store distances, angle, and index info for embedding terms
             j_counter = 0 # for tracking neighbor
             for j,offset in zip(neighbors,offsets):
@@ -195,6 +191,8 @@ class Worker:
                 rho_idx = meam.i_to_potl(jtype)
 
                 self.rhos[rho_idx].update_struct_vec_dict(rij, i)
+
+                #logging.info("WORKER: j,rij = {0},{1}".format(jtype,rij))
 
                 fj_idx = meam.i_to_potl(jtype)
 
@@ -239,20 +237,8 @@ class Worker:
 
         # TODO: ***** Ensure that everything in this function MUST be here *****
 
-        splines = self.phis + self.rhos + self.us + self.fs + self.gs
-
-        # Parse parameter vector
-        x_indices = [s.index for s in splines]
-        y_indices = [x_indices[i]+2*i for i in range(len(x_indices))]
-
-        params_split = np.split(parameters, y_indices[1:])
-
-        nphi = self.nphi
-        ntypes = self.ntypes
-
-        split_indices = [nphi, nphi+ntypes, nphi+2*ntypes, nphi+3*ntypes]
-        phi_pvecs, rho_pvecs, u_pvecs, f_pvecs, g_pvecs = np.split(
-            params_split, split_indices)
+        phi_pvecs, rho_pvecs, u_pvecs, f_pvecs, g_pvecs = \
+            self.parse_parameters(parameters)
 
         energy = 0.
 
@@ -267,10 +253,13 @@ class Worker:
         ni = np.zeros(len(self.atoms))
 
         for i in range(len(self.rhos)):
+            #logging.info("WORKER: rho_type = {0}".format(i))
             rho = self.rhos[i]
             y = rho_pvecs[i]
 
             ni += rho.compute_for_all(y)
+
+            #logging.info("WORKER: rhos = {0}".format(rho.compute_for_all(y)))
 
         # Calculate three-body contributions to ni
         for j in range(len(self.ffgs)):
@@ -295,7 +284,21 @@ class Worker:
 
             u = self.us[u_idx]
 
+            # TODO: compute_single in WorkerSpline; this is bad code
+            # TODO: clear_struct_vec(); sets struct_vec = []
+            # Compute U' value without disturbing struct_vec
+            # tmp_struct = u.struct_vecs
+            # u.struct_vecs = [[], []]
+
+            y = u_pvecs[u_idx]
             u.add_to_struct_vec(ni[i])
+            # self.uprimes[i] += u(y, 1)
+            # logging.info("WORKER: U'({0}) = {1}".format(ni[i], u(y)))
+
+            # u.struct_vecs = tmp_struct
+            #
+            # u.add_to_struct_vec(ni[i])
+            # logging.info("WORKER: U({0}) = {1}".format(ni[i], u(y)))
 
         zero_point_energy = 0
         for i in range(len(self.us)):
@@ -304,26 +307,71 @@ class Worker:
 
             # zero-point has to be calculated separately bc has to be SUBTRACTED
             # off of the energy
-            if u.struct_vec != []:
-                tmp_struct = u.struct_vec
+            if u.struct_vecs != [[],[]]:
+                tmp_struct = u.struct_vecs
 
-                u.struct_vec = []
-                for j in range(len(tmp_struct)):
-                    u.add_to_struct_vec(0)
+                u.struct_vecs = [[],[]]
+                u.add_to_struct_vec(np.zeros(len(tmp_struct[0])))
 
+                # logging.info("WORKER: zero_atom_energy = {0}".format(u(y)))
                 zero_point_energy += np.sum(u(y))
 
-                u.struct_vec = tmp_struct
+                u.struct_vecs = tmp_struct
 
-            val = np.sum(u(y))
-            energy += val
+            # logging.info("WORKER: U{0} = {1}".format(ni, u(y)))
+            energy += np.sum(u(y))
 
         return energy - zero_point_energy
 
-    def compute_forces(self, potentials):
+    def compute_forces(self, parameters):
+
+        self.compute_energies(parameters)
+
+        phi_pvecs, rho_pvecs, u_pvecs, f_pvecs, g_pvecs = \
+            self.parse_parameters(parameters)
+
         forces = np.zeros((len(self.atoms), 3))
 
+        # Pair forces
+        for i in range(len(self.phis)):
+            y = phi_pvecs[i]
+            s = self.phis[i]
+
+            forces += np.sum(s(y))
+
         return forces
+
+    def parse_parameters(self, parameters):
+        """Separates the pre-ordered 1D vector of all spline parameters into
+        groups.
+
+        Args:
+            parameters (np.arr):
+                1D array of knot points and boundary conditions for ALL
+                splines for ALL intervals
+
+        Returns:
+            *_pvecs (np.arr):
+                each return is a list of arrays of parameters. e.g.
+                phi_pvecs[0] is the parameters for the first phi spline"""
+
+        splines = self.phis + self.rhos + self.us + self.fs + self.gs
+
+        # Parse parameter vector
+        x_indices = [s.index for s in splines]
+        y_indices = [x_indices[i]+2*i for i in range(len(x_indices))]
+
+        params_split = np.split(parameters, y_indices[1:])
+
+        nphi = self.nphi
+        ntypes = self.ntypes
+
+        split_indices = [nphi, nphi+ntypes, nphi+2*ntypes, nphi+3*ntypes]
+        phi_pvecs, rho_pvecs, u_pvecs, f_pvecs, g_pvecs = np.split(
+            params_split, split_indices)
+
+        return phi_pvecs, rho_pvecs, u_pvecs, f_pvecs, g_pvecs
+
     #     """Calculates energies for all potentials using information
     #     pre-computed during initialization."""
     #     #TODO: only thing changing should be y-coords?
@@ -566,8 +614,4 @@ class Worker:
     #     self._cutoff = potentials[0].cutoff
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-
-    worker, y_vals = read_from_file('Ti_only_crowd.Ti', 'TiO.meam.spline')
-
-    logging.info("{0}".format(worker.compute_energies(y_vals)))
+    pass

src/workerSplines.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+import logging
+logger = logging.getLogger(__name__)
+
 from scipy.sparse import diags
 
 class WorkerSpline:
@@ -40,9 +43,13 @@ class WorkerSpline:
             'fixed' (fixed first derivative). If 'fixed' on one/both ends,
             self.end_derivatives cannot be None for that end
 
-        struct_vec (np.arr):
-            2D array for evaluating the spline on the structure; each row
-            corresponds to a single pair/triplet evaluation
+        struct_vecs (list [np.arr]):
+            list of structure vects, where struct_vec[i] is the structure
+            vector used to evaluate the function at the i-th derivative.
+
+            each structure vector is a 2D array for evaluating the spline on
+            the structure; each row corresponds to a single pair/triplet
+            evaluation
 
     Notes:
         This object is distinct from a spline.Spline since it requires some
@@ -63,7 +70,7 @@ class WorkerSpline:
                              "'fixed'")
 
         # Variables that can be set at beginning
-        self.x = x;
+        self.x = x
         self.h = x[1]-x[0]
 
         self.bc_type = bc_type
@@ -73,23 +80,23 @@ class WorkerSpline:
 
         # Variables that will be set at some point
         self.index = None
-        self.struct_vec = []
+        self.struct_vecs = [[], []]
 
         # Variables that will be set on evaluation
         self._y = None
         self.y1 = None
         self.end_derivs = None
 
-    def __call__(self, y):
+    def __call__(self, y, deriv=0):
 
         self.y = y
 
         z = np.concatenate((self.y, self.y1))
 
-        if self.struct_vec == []:
-            return 0.
+        if self.struct_vecs[deriv] == []:
+            return np.array([0.])
         else:
-            return self.struct_vec @ z.transpose()
+            return self.struct_vecs[deriv] @ z.transpose()
 
     # self.y made as a property to ensure setting of self.y1 and self.end_derivs
     @property
@@ -101,12 +108,16 @@ class WorkerSpline:
         self._y, self.end_derivs = np.split(y, [-2])
         self.y1 = self.M @ y.transpose()
 
-    def get_abcd(self, x):
+    def get_abcd(self, x, deriv=0):
         """Calculates the coefficients needed for spline interpolation.
 
         Args:
             x (float):
                 point at which to evaluate spline
+
+            deriv (int):
+                order of derivative to evaluate; default is zero, meaning
+                evaluate original function
 
         Returns:
             vec (np.arr):
@@ -138,74 +149,107 @@ class WorkerSpline:
 
         knots = self.x.copy()
 
-        h_00 = lambda t: (1+2*t)*(1-t)**2
-        h_10 = lambda t: t*(1-t)**2
-        h_01 = lambda t: (t**2)*(3-2*t)
-        h_11 = lambda t: (t**2)*(t-1)
-
+        # Find spline interval
         h = knots[1] - knots[0]
-
-        # Find spline interval; -1 to account for zero indexing
-        k = int(np.floor((x-knots[0])/h))
+        all_k = np.floor((x-knots[0])/h).astype(int)
 
         nknots = len(knots)
-        vec = np.zeros(2*nknots)
+        vec = np.zeros((len(x), 2*nknots))
 
-        if k < 0: # LHS extrapolation
-            k = 0
+        for i in range(len(all_k)):  # for every point to be evaluated
+            k = all_k[i]
 
-            A = 1
-            B = x - knots[0] # negative for correct sign with derivative
+            if k < 0: # LHS extrapolation
+                h_00 = np.poly1d([0,0,0,1])
+                h_10 = np.poly1d([0,0,1,0])
 
-            vec[k] = A; vec[k+nknots] = B
+                t = (x[i] - knots[0])
 
-        elif k >= nknots-1: # RHS extrapolation
-            k = nknots-1
-            C = 1
-            D = x - knots[-1]
+                h_00 = np.polyder(h_00, deriv)
+                h_10 = np.polyder(h_10, deriv)
 
-            vec[k] = C; vec[k+nknots] = D
-        else:
-            prefactor  = (knots[k+1] - knots[k])
+                A = h_00(t)
+                B = h_10(t)
 
-            t = (x - knots[k])/prefactor
+                vec[i][0] = A
+                vec[i][nknots] = B
 
-            A = h_00(t)
-            B = h_10(t)*prefactor
-            C = h_01(t)
-            D = h_11(t)*prefactor
+            elif k >= nknots-1: # RHS extrapolation
+                k = nknots-1 # centered at second-to-last knot for indexing
 
-            vec[k] = A
-            vec[k+nknots] = B
-            vec[k+1] = C
-            vec[k+1+nknots] = D
+                h_00 = np.poly1d([0,0,0,1])
+                h_10 = np.poly1d([0,0,1,0])
+
+                t = (x[i] - knots[k])
+
+                h_00 = np.polyder(h_00, deriv)
+                h_10 = np.polyder(h_10, deriv)
+
+                A = h_00(t)
+                B = h_10(t)
+
+                vec[i][k] = A
+                vec[i][-1] = B
+
+            else:
+                prefactor  = (knots[k+1] - knots[k])
+
+                h_00 = np.poly1d([2,-3,0,1])
+                h_10 = np.poly1d([1,-2,1,0])
+                h_01 = np.poly1d([-2,3,0,0])
+                h_11 = np.poly1d([1,-1,0,0])
+
+                t = (x[i] - knots[k])/prefactor
+
+                h_00 = np.polyder(h_00, deriv)
+                h_10 = np.polyder(h_10, deriv)
+                h_01 = np.polyder(h_01, deriv)
+                h_11 = np.polyder(h_11, deriv)
+
+                A = h_00(t)
+                B = h_10(t)*prefactor
+                C = h_01(t)
+                D = h_11(t)*prefactor
+
+                vec[i][k] = A
+                vec[i][k+nknots] = B
+                vec[i][k+1] = C
+                vec[i][k+1+nknots] = D
+
+                if deriv == 0:
+                    scaling = 1
+                else:
+                    scaling = 1/(prefactor*deriv)
+
+                vec[i] *= scaling
 
         return vec
 
     def add_to_struct_vec(self, val):
+        """Builds the ABCD vectors for all elements in val, then adds to
+        struct_vec
 
-        abcd = self.get_abcd(val)
+        Args:
+            val (int, float, np.arr, list):
+                collection of values to add; converted into a np.arr in this
+                function"""
 
-        if self.struct_vec == []:
-            self.struct_vec = abcd.reshape((1,len(abcd)))
+        add = np.atleast_1d(val)
+
+        abcd_0 = self.get_abcd(add, 0)
+        abcd_1 = self.get_abcd(add, 1)
+
+        # Add to struct_vec for normal eval
+        if self.struct_vecs[0] == []:
+            self.struct_vecs[0] = abcd_0
         else:
-            self.struct_vec = np.vstack((self.struct_vec, abcd))
+            self.struct_vecs[0] = np.vstack((self.struct_vecs[0], abcd_0))
 
-    def get_y2(self):
-        # TODO: is this needed at all?
-        if self.y is None:
-            raise ValueError("y must be set first")
-
-        y2 = np.zeros(len(self.x))
-
-        # internal knots and rhs:
-        for i in range(1,len(y2)):
-            y2[i] = 6*self.x[i-1] + self.h*2*self.y1[i-1] - 6*self.x[i] + self.h*4*self.y1[i]
-
-        # lhs
-        y2[0] = -6*self.x[0] - self.h*4*self.y1[0] + 6*self.x[1] - self.h*2*self.y1[1]
-
-        return y2
+        # Add to struct_vec for deriv eval
+        if self.struct_vecs[1] == []:
+            self.struct_vecs[1] = abcd_1
+        else:
+            self.struct_vecs[1] = np.vstack((self.struct_vecs[1], abcd_1))
 
     def plot(self, fname=''):
         raise NotImplementedError("Worker plotting is not ready yet")
@@ -220,8 +264,8 @@ class WorkerSpline:
         plt.figure()
         plt.plot(self.x, self.y, 'ro', label='knots')
 
-        tmp_struct = self.struct_vec
-        self.struct_vec = None
+        tmp_struct = self.struct_vecs
+        self.struct_vecs = None
 
         plot_x = np.linspace(low,high,1000)
 
@@ -230,7 +274,7 @@ class WorkerSpline:
 
         plot_y = self(np.concatenate((self.y, self.end_derivs)))
 
-        self.struct_vec = tmp_struct
+        self.struct_vecs = tmp_struct
 
         plt.plot(plot_x, plot_y)
         plt.legend()
@@ -257,21 +301,27 @@ class RhoSpline(WorkerSpline):
 
         self.natoms = natoms
         # TODO: could convert to numpy array rather than dict?
-        self.struct_vec_dict = dict.fromkeys(np.arange(natoms), [])
+        self.struct_vec_dict = {key:[[], []] for key in np.arange(natoms)}
 
-    def compute_for_all(self, y):
+    def compute_for_all(self, y, deriv=0):
         """Computes results for every struct_vec in struct_vec_dict
 
         Returns:
             ni (np.arr):
-                each entry is the set of all ni for a specific atom"""
+                each entry is the set of all ni for a specific atom
+
+            deriv (int):
+                which derivative to evaluate"""
 
         ni = np.zeros(self.natoms)
 
         for i in self.struct_vec_dict.keys():
-            self.struct_vec = self.struct_vec_dict[i]
+            self.struct_vecs = self.struct_vec_dict[i]
 
-            ni[i] = np.sum(np.array(super(RhoSpline, self).__call__(y)))
+            val = super(RhoSpline, self).__call__(y, deriv)
+            np.set_printoptions(precision=16)
+            #logging.info("WORKER: i, val = {0}, {1}".format(i, val, digits=15))
+            ni[i] = np.sum(np.array(super(RhoSpline, self).__call__(y, deriv)))
 
         return ni
 
@@ -285,15 +335,24 @@ class RhoSpline(WorkerSpline):
             atom_id (int):
                 atom id"""
 
-        abcd = self.get_abcd(val)
+        add = np.atleast_1d(val)
 
-        if self.struct_vec_dict[atom_id] == []:
-            self.struct_vec_dict[atom_id] = abcd.reshape((1,len(abcd)))
+        abcd_0 = self.get_abcd(add, 0)
+        abcd_1 = self.get_abcd(add, 1)
+
+        if self.struct_vec_dict[atom_id][0] == []:
+            self.struct_vec_dict[atom_id][0] = abcd_0
         else:
-            struct_vec = np.vstack((self.struct_vec_dict[atom_id], abcd))
-            self.struct_vec_dict[atom_id] = struct_vec
+            struct_vec = np.vstack((self.struct_vec_dict[atom_id][0], abcd_0))
+            self.struct_vec_dict[atom_id][0] = struct_vec
 
-class ffgSpline():
+        if self.struct_vec_dict[atom_id][1] == []:
+            self.struct_vec_dict[atom_id][1] = abcd_1
+        else:
+            struct_vec = np.vstack((self.struct_vec_dict[atom_id][1], abcd_1))
+            self.struct_vec_dict[atom_id][1] = struct_vec
+
+class ffgSpline:
     """Spline representation specifically used for building a spline
     representation of the combined funcions f_j*f_k*g_jk used in the
     three-body interactions."""
@@ -315,11 +374,12 @@ class ffgSpline():
 
         self.natoms = natoms
         # TODO: could convert to numpy array rather than dict?
-        self.struct_vec_dict = dict.fromkeys(np.arange(natoms), [])
+        self.struct_vec_dict = {key:[[], []] for key in np.arange(natoms)}
+        self.struct_vecs = [[], []]
 
-    def __call__(self, y_fj, y_fk, y_g):
+    def __call__(self, y_fj, y_fk, y_g, deriv=0):
 
-        if self.struct_vec == []:
+        if self.struct_vecs[deriv] == []:
             return 0.
 
         self.fj.y = y_fj
@@ -333,28 +393,33 @@ class ffgSpline():
 
         self.y = np.prod(cartesian_product(fj_vec, fk_vec, g_vec), axis=1)
 
-        return self.struct_vec @ self.y
+        return self.struct_vecs[deriv] @ self.y
 
-    def compute_for_all(self, y_fj, y_fk, y_g):
+    def compute_for_all(self, y_fj, y_fk, y_g, deriv=0):
         """Computes results for every struct_vec in struct_vec_dict
 
         Returns:
             ni (list [np.arr]):
-                each entry is the set of all ni for a specific atom"""
+                each entry is the set of all ni for a specific atom
+
+            deriv (int):
+                derivative at which to evaluate the function"""
 
         ni = np.zeros(self.natoms)
 
         for i in self.struct_vec_dict.keys():
-            self.struct_vec = self.struct_vec_dict[i]
+            self.struct_vecs = self.struct_vec_dict[i]
 
-            val = np.sum(self.__call__(y_fj, y_fk, y_g))
+            val = np.sum(self.__call__(y_fj, y_fk, y_g, deriv))
 
             ni[i] = val
 
         return ni
 
-    def get_abcd(self, rij, rik, cos_theta):
-        """Computes the full parameter vector for the multiplication of ffg
+    def get_abcd(self, rij, rik, cos_theta, deriv=0):
+        """Ovverrides WorkerSpline.get_abcd()
+
+        Computes the full parameter vector for the multiplication of ffg
         splines
 
         Args:
@@ -365,11 +430,24 @@ class ffgSpline():
                 the value at which to evaluate fk
 
             cos_theta (float):
-                the value at which to evaluate g"""
+                the value at which to evaluate g
 
-        fj_abcd = self.fj.get_abcd(rij)
-        fk_abcd = self.fk.get_abcd(rik)
-        g_abcd = self.g.get_abcd(cos_theta)
+            deriv (int):
+                deriv at which to evaluate the splines"""
+
+        add_rij = np.atleast_1d(rij)
+        add_rik = np.atleast_1d(rik)
+        add_cos_theta = np.atleast_1d(cos_theta)
+
+        # TODO: using ravel() b/c multi-value is not ready for ffgSpline
+        fj_abcd = self.fj.get_abcd(add_rij, deriv)
+        fj_abcd = np.ravel(fj_abcd)
+
+        fk_abcd = self.fk.get_abcd(add_rik, deriv)
+        fk_abcd = np.ravel(fk_abcd)
+
+        g_abcd = self.g.get_abcd(add_cos_theta, deriv)
+        g_abcd = np.ravel(g_abcd)
 
         full_abcd = np.prod(cartesian_product(fj_abcd, fk_abcd, g_abcd), axis=1)
 
@@ -392,13 +470,20 @@ class ffgSpline():
             atom_id (int):
                 atom id"""
 
-        abcd = self.get_abcd(rij, rik, cos_theta)
+        abcd_0 = self.get_abcd(rij, rik, cos_theta, 0)
+        abcd_1 = self.get_abcd(rij, rik, cos_theta, 1)
 
-        if self.struct_vec_dict[atom_id] == []:
-            self.struct_vec_dict[atom_id] = abcd.reshape((1,len(abcd)))
+        if self.struct_vec_dict[atom_id][0] == []:
+            self.struct_vec_dict[atom_id][0] = abcd_0.reshape((1,len(abcd_0)))
         else:
-            struct_vec = np.vstack((self.struct_vec_dict[atom_id], abcd))
-            self.struct_vec_dict[atom_id] = struct_vec
+            struct_vec = np.vstack((self.struct_vec_dict[atom_id][0], abcd_0))
+            self.struct_vec_dict[atom_id][0] = struct_vec
+
+        if self.struct_vec_dict[atom_id][1] == []:
+            self.struct_vec_dict[atom_id][1] = abcd_1.reshape((1,len(abcd_1)))
+        else:
+            struct_vec = np.vstack((self.struct_vec_dict[atom_id][1], abcd_1))
+            self.struct_vec_dict[atom_id][1] = struct_vec
 
 def build_M(num_x, dx, bc_type):
     """Builds the A and B matrices that are needed to find the function

tests/TestWorker.py
@@ -16,23 +16,23 @@ from tests.testStructs import dimers, trimers, bulk_vac_ortho, \
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
-logging.disable(logging.CRITICAL)
+# logging.disable(logging.CRITICAL)
 
-EPS = 1e-15
+EPS = 1e-16
 np.random.seed(42)
 
 N = 1
 
 # Flags for what tests to run
-energy_flag = True*0
-forces_flag = True*1
+energy_flag = True*1
+forces_flag = True*0
 
-zero_pots_flag  = True*1
-const_pots_flag = True*1
-rand_pots_flag  = True*0
+zero_pots_flag  = True*0
+const_pots_flag = True*0
+rand_pots_flag  = True*1
 
-meam_flag       = True*0
-phionly_flag    = True*1
+meam_flag       = True*1
+phionly_flag    = True*0
 rhophi_flag     = True*0
 nophi_flag      = True*0
 rho_flag        = True*0
@@ -41,7 +41,7 @@ norhophi_flag   = True*0
 
 dimers_flag  = True*1
 trimers_flag = True*1
-bulk_flag    = True*0
+bulk_flag    = True*1
 
 allstructs = {}
 
@@ -53,7 +53,26 @@ if bulk_flag:
     allstructs = {**allstructs, **bulk_vac_ortho, **bulk_periodic_ortho,
                   **bulk_vac_rhombo, **bulk_periodic_rhombo, **extra}
 
-# allstructs = {'aba':trimers['aba']}
+# rzm: still incorrect comparing Worker to LAMMPS; MEAM acc to LAMMPS 1e-8
+
+# allstructs = {'aaa':trimers['aaa']}
+# allstructs = {'bulk_vac_ortho_type1':bulk_vac_ortho['bulk_vac_ortho_type1']}
+# allstructs = {'8_atoms':extra['8_atoms']}
+
+import ase.build
+from tests.testVars import a0, vac
+
+i = 3
+atoms = ase.build.bulk('H', crystalstructure='fcc', a=a0, orthorhombic=True)
+atoms = atoms.repeat((i,i,1))
+atoms.rattle()
+atoms.center(vacuum=0)
+atoms.set_pbc(True)
+atoms.set_chemical_symbols(np.random.randint(1,3, size=len(atoms)))
+
+atoms.center(vacuum=vac)
+
+allstructs = {'dummy':atoms}
 
 ################################################################################
 # Helper functions
@@ -104,7 +123,7 @@ def getLammpsResults(pots, structs):
 
             # cstart = time.time()
             results = p.get_lammps_results(atoms)
-            energies[name][pnum] = results['energy']
+            energies[name][pnum] = results['energy']/len(atoms)
             forces[name].append(results['forces'])
             # lammps_calcduration += float(time.time() - cstart)
 
@@ -126,7 +145,7 @@ def runner_energy(pots, structs):
         w = Worker(atoms, x_pvec, indices, pots[0].types)
         start = time.time()
         # TODO: to optimize, preserve workers for each struct
-        energies[name] = w.compute_energies(y_pvec)
+        energies[name] = w.compute_energies(y_pvec)/len(atoms)
         # py_calcduration += time.time() - start
 
     return energies
@@ -163,13 +182,13 @@ if zero_pots_flag:
 
         @parameterized.expand(loader_energy('', calc_energies, energies))
         def test_zero_potential_energy(name, a, b):
-            np.testing.assert_allclose(a,b,atol=EPS)
+            np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
     if forces_flag:
         calc_forces = runner_forces([p], allstructs)
         @parameterized.expand(loader_forces('',calc_forces, forces))
         def test_zero_potential_forces(name, a, b):
-            np.testing.assert_allclose(a,b,atol=EPS)
+            np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 ################################################################################
 # TODO: const_pot test needs multiple potentials at once
 if const_pots_flag:
@@ -186,14 +205,14 @@ if const_pots_flag:
 
             @parameterized.expand(loader_energy('', calc_energies, energies))
             def test_constant_potential_meam_energy(name, a, b):
-               np.testing.assert_allclose(a,b,atol=EPS)
+               np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
         if forces_flag:
             calc_forces = runner_forces([p], allstructs)
 
             @parameterized.expand(loader_forces('', calc_forces, forces))
             def test_constant_potential_meam_forces(name, a, b):
-                np.testing.assert_allclose(a,b,atol=EPS)
+                np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
     if phionly_flag:
         """phionly subtype"""
@@ -204,14 +223,14 @@ if const_pots_flag:
 
             @parameterized.expand(loader_energy('', calc_energies, energies))
             def test_constant_potential_phionly_energy(name, a, b):
-               np.testing.assert_allclose(a,b,atol=EPS)
+               np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
         if forces_flag:
             calc_forces = runner_forces([p.phionly_subtype()], allstructs)
 
             @parameterized.expand(loader_forces('', calc_forces, forces))
             def test_constant_potential_phionly_forces(name, a, b):
-                np.testing.assert_allclose(a,b,atol=EPS)
+                np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
     if rhophi_flag:
         """rhophi subtype"""
@@ -222,14 +241,14 @@ if const_pots_flag:
 
             @parameterized.expand(loader_energy('', calc_energies, energies))
             def test_constant_potential_rhophi_energy(name, a, b):
-               np.testing.assert_allclose(a,b,atol=EPS)
+               np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
         if forces_flag:
             calc_forces = runner_forces([p.rhophi_subtype()], allstructs)
 
             @parameterized.expand(loader_forces('', calc_forces, forces))
             def test_constant_potential_rhophi_forces(name, a, b):
-                np.testing.assert_allclose(a,b,atol=EPS)
+                np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
     if nophi_flag:
         """nophi subtype"""
@@ -240,14 +259,14 @@ if const_pots_flag:
 
             @parameterized.expand(loader_energy('', calc_energies, energies))
             def test_constant_potential_nophi_energy(name, a, b):
-               np.testing.assert_allclose(a,b,atol=EPS)
+               np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
         if forces_flag:
             calc_forces = runner_forces([p.nophi_subtype()], allstructs)
 
             @parameterized.expand(loader_forces('', calc_forces, forces))
             def test_constant_potential_nophi_forces(name, a, b):
-                np.testing.assert_allclose(a,b,atol=EPS)
+                np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
     if rho_flag:
         """rho subtype"""
@@ -258,14 +277,14 @@ if const_pots_flag:
 
             @parameterized.expand(loader_energy('', calc_energies, energies))
             def test_constant_potential_rho_energy(name, a, b):
-                np.testing.assert_allclose(a,b,atol=EPS)
+                np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
         if forces_flag:
             calc_forces = runner_forces([p.rho_subtype()], allstructs)
 
             @parameterized.expand(loader_forces('', calc_forces, forces))
             def test_constant_potential_rho_forces(name, a, b):
-                np.testing.assert_allclose(a,b,atol=EPS)
+                np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
     if norho_flag:
         """norho subtype"""
@@ -276,14 +295,14 @@ if const_pots_flag:
 
             @parameterized.expand(loader_energy('', calc_energies, energies))
             def test_constant_potential_norho_energy(name, a, b):
-               np.testing.assert_allclose(a,b,atol=EPS)
+               np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
         if forces_flag:
             calc_forces = runner_forces([p.norho_subtype()], allstructs)
 
             @parameterized.expand(loader_forces('', calc_forces, forces))
             def test_constant_potential_norho_forces(name, a, b):
-                np.testing.assert_allclose(a,b,atol=EPS)
+                np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
     if norhophi_flag:
         """norhophi subtype"""
@@ -294,14 +313,14 @@ if const_pots_flag:
 
             @parameterized.expand(loader_energy('', calc_energies, energies))
             def test_constant_potential_norhophi_energy(name, a, b):
-                np.testing.assert_allclose(a,b,atol=EPS)
+                np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
         if forces_flag:
             calc_forces = runner_forces([p.norhophi_subtype()], allstructs)
 
             @parameterized.expand(loader_forces('', calc_forces, forces))
             def test_constant_potential_norhophi_forces(name, a, b):
-                np.testing.assert_allclose(a,b,atol=EPS)
+                np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
     #################################################################################
 if rand_pots_flag:
     """Random potentials"""
@@ -317,14 +336,16 @@ if rand_pots_flag:
 
             @parameterized.expand(loader_energy('', calc_energies, energies))
             def test_random_potential_meam_energy(name, a, b):
-               np.testing.assert_allclose(a,b,atol=EPS)
+               logging.info("WORKER energy = {0}".format(a))
+               logging.info("LAMMPS energy = {0}".format(b))
+               np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
         if forces_flag:
             calc_forces = runner_forces(p, allstructs)
 
             @parameterized.expand(loader_forces('', calc_forces, forces))
             def test_random_potential_meam_forces(name, a, b):
-                np.testing.assert_allclose(a,b,atol=EPS)
+                np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
     if phionly_flag:
         """phionly subtype"""
@@ -337,14 +358,16 @@ if rand_pots_flag:
 
             @parameterized.expand(loader_energy('', calc_energies, energies))
             def test_random_potential_phionly_energy(name, a, b):
-                np.testing.assert_allclose(a,b,atol=EPS)
+                logging.info("WORKER energy = {0}".format(a))
+                logging.info("LAMMPS energy = {0}".format(b))
+                np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
         if forces_flag:
             calc_forces = runner_forces(p, allstructs)
 
             @parameterized.expand(loader_forces('', calc_forces, forces))
             def test_random_potential_phionly_forces(name, a, b):
-                np.testing.assert_allclose(a,b,atol=EPS)
+                np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
     if rhophi_flag:
         """rhophi subtype"""
@@ -357,14 +380,14 @@ if rand_pots_flag:
 
             @parameterized.expand(loader_energy('', calc_energies, energies))
             def test_random_potential_rhophi_energy(name, a, b):
-               np.testing.assert_allclose(a,b,atol=EPS)
+               np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
         if forces_flag:
             calc_forces = runner_forces(p, allstructs)
 
             @parameterized.expand(loader_forces('', calc_forces, forces))
             def test_random_potential_rhophi_forces(name, a, b):
-                np.testing.assert_allclose(a,b,atol=EPS)
+                np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
     if nophi_flag:
         """nophi subtype"""
@@ -377,14 +400,14 @@ if rand_pots_flag:
 
             @parameterized.expand(loader_energy('', calc_energies, energies))
             def test_random_potential_nophi_energy(name, a, b):
-               np.testing.assert_allclose(a,b,atol=EPS)
+               np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
         if forces_flag:
             calc_forces = runner_forces(p, allstructs)
 
             @parameterized.expand(loader_forces('', calc_forces, forces))
             def test_random_potential_nophi_forces(name, a, b):
-                np.testing.assert_allclose(a,b,atol=EPS)
+                np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
     if rho_flag:
         """rho subtype"""
@@ -398,14 +421,14 @@ if rand_pots_flag:
 
             @parameterized.expand(loader_energy('', calc_energies, energies))
             def test_random_potential_rho_energy(name, a, b):
-               np.testing.assert_allclose(a,b,atol=EPS)
+               np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
         if forces_flag:
             calc_forces = runner_forces(p, allstructs)
 
             @parameterized.expand(loader_forces('', calc_forces, forces))
             def test_random_potential_rho_forces(name, a, b):
-                np.testing.assert_allclose(a,b,atol=EPS)
+                np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
     if norho_flag:
         """norho subtype"""
@@ -418,14 +441,14 @@ if rand_pots_flag:
 
             @parameterized.expand(loader_energy('', calc_energies, energies))
             def test_random_potential_norho_energy(name, a, b):
-               np.testing.assert_allclose(a,b,atol=EPS)
+               np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
         if forces_flag:
             calc_forces = runner_forces(p, allstructs)
 
             @parameterized.expand(loader_forces('', calc_forces, forces))
             def test_random_potential_norho_forces(name, a, b):
-                np.testing.assert_allclose(a,b,atol=EPS)
+                np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
     if norhophi_flag:
         """norhophi subtype"""
@@ -439,13 +462,13 @@ if rand_pots_flag:
 
             @parameterized.expand(loader_energy('', calc_energies, energies))
             def test_random_potential_norhophi_energy(name, a, b):
-               np.testing.assert_allclose(a,b,atol=EPS)
+               np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
         if forces_flag:
             calc_forces = runner_forces(p, allstructs)
 
             @parameterized.expand(loader_forces('', calc_forces, forces))
             def test_random_potential_norhophi_forces(name, a, b):
-                np.testing.assert_allclose(a,b,atol=EPS)
+                np.testing.assert_allclose(a,b,atol=EPS,rtol=0)
 
 ################################################################################

tests/TestWorkerSplines.py
@@ -6,71 +6,72 @@ import workerSplines
 from spline import Spline
 from workerSplines import WorkerSpline, RhoSpline
 
-DIGITS = 8
-EPS = 1e-8
+DIGITS = 15
+EPS = 1e-15
 
 class WorkerSplineTests(unittest.TestCase):
 
     def setUp(self):
-        self.x = np.linspace(-1, 1, 5)
+        # self.x = np.linspace(-1, 1, 5)
+        self.x = np.array([-1, -0.5, 0, 0.5, 1])
         self.dx = self.x[1] - self.x[0]
 
-        d0 = dN = 0
-        self.y = np.array([ 0.05138434,  0.01790244, -0.26065088, -0.19016379,
-                       -0.76379542, d0, dN])
+        self.y = np.array([1, .25, 0, .25, 1, -2, 2])
 
     def test_y_setter(self):
+
         ws = WorkerSpline(self.x, ('fixed', 'fixed'))
 
         ws.y = self.y
 
         np.testing.assert_allclose(ws.y, self.y[:-2], atol=EPS, rtol=0)
         np.testing.assert_allclose(ws.end_derivs, self.y[-2:], atol=EPS, rtol=0)
-        np.testing.assert_allclose(ws.y1, np.array([ 0., -0.46622228,
-                             -0.00732221, -0.75288626, 0.]), atol=EPS,rtol=0)
+        np.testing.assert_allclose(ws.y1, np.array([-2,-1,0,1,2]))
 
     def test_full_eval(self):
         d0, dN = self.y[-2:]
 
         ws = WorkerSpline(self.x, ('fixed', 'fixed'))
-        cs = Spline(self.x, self.y[:-2], bc_type=((1,d0), (1,dN)))
+        cs = Spline(self.x, self.y[:-2], bc_type=((1,d0), (1,dN)),
+                    end_derivs=(d0,dN))
 
         test_x = np.linspace(-10, 20, 1000)
 
-        for i in range(len(test_x)):
-            ws.add_to_struct_vec(test_x[i])
+        ws.add_to_struct_vec(test_x)
 
         results = ws(self.y)
 
         for i in range(len(test_x)):
+            print(test_x[i])
             self.assertAlmostEqual(results[i], cs(test_x[i]), DIGITS)
 
     def test_rhs_extrap(self):
         d0, dN = self.y[-2:]
 
         ws = WorkerSpline(self.x, ('fixed', 'fixed'))
-        cs = Spline(self.x, self.y[:-2], bc_type=((1,d0), (1,dN)))
+        cs = Spline(self.x, self.y[:-2], bc_type=((1,d0), (1,dN)),
+                    end_derivs=(d0,dN))
 
-        test_x = np.linspace(self.x[-1], self.x[-1]*1.5, 1000)
+        test_x = np.linspace(self.x[-1], self.x[-1]+2, 100)
+        # test_x = np.array([1.5])
 
-        for i in range(len(test_x)):
-            ws.add_to_struct_vec(test_x[i])
+        ws.add_to_struct_vec(test_x)
 
         results = ws(self.y)
 
         for i in range(len(test_x)):
-            self.assertAlmostEqual(results[i], cs(test_x[i]))
+            self.assertAlmostEqual(results[i], cs(test_x[i]), DIGITS)
 
     def test_lhs_extrap(self):
         d0, dN = self.y[-2:]
 
         ws = WorkerSpline(self.x, ('fixed', 'fixed'))
-        cs = Spline(self.x, self.y[:-2], bc_type=((1,d0), (1,dN)))
+        cs = Spline(self.x, self.y[:-2], bc_type=((1,d0), (1,dN)),
+                    end_derivs=(d0,dN))
 
-        test_x = np.linspace(self.x[0]*0.5, self.x[0], 1000)
+        test_x = np.linspace(self.x[0]-2, self.x[0], 1000)
 
-        for i in range(len(test_x)):
-            ws.add_to_struct_vec(test_x[i])
+        ws.add_to_struct_vec(test_x)
 
         results = ws(self.y)
 
@@ -99,7 +100,7 @@ class WorkerSplineTests(unittest.TestCase):
         ws = WorkerSpline(self.x, ('fixed', 'fixed'))
         cs = Spline(self.x, self.y[:-2], bc_type=((1,d0), (1,dN)))
 
-        test_x = np.linspace(self.x[0], self.x[1], 1000)
+        test_x = np.linspace(self.x[0], self.x[1], 100)
 
         for i in range(len(test_x)):
             ws.add_to_struct_vec(test_x[i])
@@ -115,19 +116,14 @@ class WorkerSplineTests(unittest.TestCase):
         ws = WorkerSpline(self.x, ('fixed', 'fixed'))
         cs = Spline(self.x, self.y[:-2], bc_type=((1,d0), (1,dN)))
 
-        test_x = np.linspace(self.x[-2], self.x[-1], 1000)
+        test_x = np.linspace(self.x[-2], self.x[-1]-0.01, 100)
 
-        for i in range(len(test_x)):
-            ws.add_to_struct_vec(test_x[i])
-
-        results = ws(self.y)
-        results = np.zeros(test_x.shape)
-        for i in range(len(test_x)):
-            ws.add_to_struct_vec(test_x[i])
+        ws.add_to_struct_vec(test_x)
 
         results = ws(self.y)
 
         for i in range(len(test_x)):
+            print(i)
             self.assertAlmostEqual(results[i], cs(test_x[i]))
 
     def test_constructor_bad_x(self):
@@ -178,17 +174,16 @@ class WorkerSplineTests(unittest.TestCase):
 
         self.assertEqual(ws(y), 0.)
 
-    def test_eval_sloped(self):
+    def test_eval_internal_sloped(self):
         x = np.arange(10, dtype=float)
         y = np.arange(12, dtype=float); y[-2] = y[-1] = 1
 
-        test_x = np.linspace(0, 10, 100)
+        test_x = np.linspace(0, 9, 100)
 
         # TODO: should be same with 'nat'
         ws = WorkerSpline(x, ('fixed', 'fixed'))
 
-        for el in test_x:
-            ws.add_to_struct_vec(el)
+        ws.add_to_struct_vec(test_x)
 
         for i in range(100):
             np.testing.assert_allclose(np.sum(ws(y)), np.sum(test_x), atol=EPS, rtol=0)
@@ -208,8 +203,9 @@ class WorkerSplineTests(unittest.TestCase):
 
         ws = WorkerSpline(x, ('fixed', 'fixed'))
         ws.add_to_struct_vec(-1.)
+        ws.add_to_struct_vec(-2)
 
-        self.assertEqual(ws(y), -1.)
+        np.testing.assert_allclose(ws(y), [-1., -2])
 
     def test_eval_flat_rhs_extrap(self):
         x = np.arange(10, dtype=float)
@@ -229,42 +225,64 @@ class WorkerSplineTests(unittest.TestCase):
 
         self.assertEqual(ws(y), 10.)
 
-    def test_eval_sin_fxn(self):
-        pass
-
     def test_build_M_natural_natural(self):
-        M = workerSplines.build_M(len(self.x), self.dx, bc_type=('natural',
-                                                                'natural'))
+        d0 = dN = 0
+        x = np.array([-1, -0.5, 0, 0.5, 1])
+        dx = x[1]-x[0]
+
+        y = np.array([ 0.05138434,  0.01790244, -0.26065088, -0.19016379,
+                            -0.76379542, d0, dN])
+
+        M = workerSplines.build_M(len(x), dx, bc_type=('natural', 'natural'))
 
         true = np.array([ 0.13719161, -0.47527463, -0.10830441, -0.33990513,
                           -1.55094231])
 
-        np.testing.assert_allclose(true, M@self.y, atol=1e-7, rtol=0)
+        np.testing.assert_allclose(true, M@y, atol=1e-7, rtol=0)
 
     def test_build_M_natural_fixed(self):
-        M = workerSplines.build_M(len(self.x), self.dx, bc_type=('natural',
-                                                                'fixed'))
+        d0 = dN = 0
+        x = np.linspace(-1,1,5)
+        dx = x[1]-x[0]
+
+        y = np.array([ 0.05138434,  0.01790244, -0.26065088, -0.19016379,
+                            -0.76379542, d0, dN])
+
+        M = workerSplines.build_M(len(x), dx, bc_type=('natural', 'fixed'))
 
         true = np.array([0.15318071, -0.50725283, 0.00361927, -0.75562163, 0.])
 
-        np.testing.assert_allclose(true, M@self.y, atol=EPS, rtol=0)
+        np.set_printoptions(precision=15)
+        np.testing.assert_allclose(true, M@y, atol=1e-8, rtol=0)
 
     def test_build_M_fixed_natural(self):
-        M = workerSplines.build_M(len(self.x), self.dx, bc_type=('fixed',
-                                                                'natural'))
+        d0 = dN = 0
+        x = np.array([-1, -0.5, 0, 0.5, 1])
+        dx = x[1]-x[0]
+
+        y = np.array([ 0.05138434,  0.01790244, -0.26065088, -0.19016379,
+                            -0.76379542, d0, dN])
+
+        M = workerSplines.build_M(len(x), dx, bc_type=('fixed', 'natural'))
 
         true = np.array([0, -0.43850162, -0.11820483, -0.33707643, -1.55235667])
 
-        np.testing.assert_allclose(true, M@self.y, atol=EPS, rtol=0)
+        np.testing.assert_allclose(true, M@y, atol=1e-8, rtol=0)
 
     def test_build_M_fixed_fixed(self):
-        M = workerSplines.build_M(len(self.x), self.dx, bc_type=('fixed',
+        d0 = dN = 0
+        x = np.array([-1, -0.5, 0, 0.5, 1])
+        dx = x[1]-x[0]
+        y = np.array([ 0.05138434,  0.01790244, -0.26065088, -0.19016379,
+                            -0.76379542, d0, dN])
+
+        M = workerSplines.build_M(len(x), dx, bc_type=('fixed',
                                                                 'fixed'))
 
         true = np.array([0.00000000e+00, -4.66222277e-01, -7.32221143e-03,
                             -7.52886257e-01, -8.88178420e-16])
 
-        np.testing.assert_allclose(true, M@self.y, atol=EPS, rtol=0)
+        np.testing.assert_allclose(true, M@y, atol=1e-8, rtol=0)
 
     def test_build_M_bad_LHS(self):
         self.assertRaises(ValueError, workerSplines.build_M, len(self.x),
@@ -289,6 +307,87 @@ class WorkerSplineTests(unittest.TestCase):
         ws.add_to_struct_vec(0)
 
         self.assertEqual(np.sum(ws(y)), 3)
+
+class WorkerSplineVsMEAMTests(unittest.TestCase):
+
+    def setUp(self):
+
+        import numpy as np
+        import meam
+        from worker import Worker
+        from tests.testPotentials import get_random_pots
+
+        np.random.seed(42)
+        p = get_random_pots(1)['meams'][0]
+
+        self.m_phis = p.phis
+        self.m_rhos = p.rhos
+        self.m_us = p.us
+        self.m_fs = p.fs
+        self.m_gs = p.gs
+
+        x_pvec, y_pvec, x_indices = meam.splines_to_pvec(p.splines)
+
+        y_indices = [x_indices[i]+2*i for i in range(len(x_indices))]
+
+
+
+        params_split = np.split(y_pvec, y_indices[1:])
+
+        nphi = 3
+        ntypes = 2
+
+        split_indices = [nphi, nphi+ntypes, nphi+2*ntypes, nphi+3*ntypes]
+
+        self.phi_ys, self.rho_ys, self.u_ys, self.f_ys, self.g_ys = np.split(
+            params_split, split_indices)
+
+        ntypes = 2
+        nphi = 3
+
+        knots_split = np.split(x_pvec, x_indices[1:])
+
+        splines = []
+
+        for i in range(ntypes*(ntypes+4)):
+            idx = x_indices[i]
+
+            bc_type = ('fixed', 'fixed')
+
+            s = WorkerSpline(knots_split[i], bc_type)
+
+            s.index = idx
+
+            splines.append(s)
+
+        split_indices = np.array([nphi, nphi+ntypes, nphi+2*ntypes,
+                                 nphi+3*ntypes])
+        self.w_phis, self.w_rhos, self.w_us, self.w_fs, self.w_gs =\
+            np.split(splines, split_indices)
+
+        self.w_phis = list(self.w_phis)
+        self.w_rhos = list(self.w_rhos)
+        self.w_us = list(self.w_us)
+        self.w_fs = list(self.w_fs)
+        self.gs = list(self.w_gs)
+
+    def test_phis(self):
+        for i in range(len(self.w_phis)):
+            w_phi = self.w_phis[i]
+            m_phi = self.m_phis[i]
+
+            test_vals = np.linspace(w_phi.x[0]-1, w_phi.x[-1]+1, 100)
+
+            w_phi.add_to_struct_vec(test_vals)
+            y_pvec = self.phi_ys[i]
+
+            w_results = w_phi(y_pvec)
+
+            for j in range(100):
+                # print(test_vals[j])
+                print(j)
+                if j == 86:
+                    self.assertAlmostEqual(w_results[j], m_phi(test_vals[j]), DIGITS)
 
 class RhoSplineTests(unittest.TestCase):
 