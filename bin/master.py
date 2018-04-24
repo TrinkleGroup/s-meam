3c825ade8a15363fc28a5c6686537f9401dd1d9a
Josh Vita <vita.joshua@gmail.com>
2018-04-24 09:02:09 -0500

    4-24: worker serialization

--- bin/master.py
+++ bin/master.py
@@ -1,12 +1,17 @@
 import sys
 sys.path.insert(0, './')
 
+import numpy as np
+seed = 42
+np.random.seed(seed)
+print("Seed value: {0}\n".format(seed))
+
 import os
 import psutil
 import time
 import glob
 import ctypes
-import numpy as np
+import pickle
 import multiprocessing as multi
 from itertools import repeat
 from pprint import pprint
@@ -50,8 +55,8 @@ def get_structure_list():
 
     #return list(dct.keys())#, list(dct.values())
     val = ["data/test_db/data.bulk_periodic_rhombo_type2"]*num_copies
-    val = ["data/test_db/data.aaa"]*num_copies
-    val = glob.glob("data/test_db/data.*")
+    val = ["data/seed_42/evaluator.aaa"]*num_copies
+    val = glob.glob("data/seed_42/evaluator.*")[:1]
 
     return val
 
@@ -76,13 +81,8 @@ if __name__ == "__main__":
     print("\t3) Comparing PER-ATOM values")
     print()
 
-    seed = 42
-    np.random.seed(seed)
-    print("Seed value: {0}\n".format(seed))
-
     # load potential info (should be passed in)
     knots, knot_idxs, types = nodes_should_get_initialized_with_this_potential_info()
-    #num_params = 2*(len(knots) + 2)
     num_splines = len(knot_idxs)
 
     # load structure info (should also be passed in)
@@ -96,8 +96,17 @@ if __name__ == "__main__":
     parameter_array = np.random.random(knots.shape[0] + 2*num_splines)
     parameter_array = np.atleast_2d(parameter_array)
 
-    # prepare parallel stuff
-    num_procs = 7
+    # build evaluators
+    workers = [pickle.load(open(path, 'rb')) for path in struct_names]
+
+    num_procs = 10
+    num_procs = multi.cpu_count()
+
+    if len(workers) > num_procs:
+        raise ValueError("num_workers should not exceed num_processors")
+
+    num_procs = len(workers)
+
     print("Requesting {0}/{1} processors".format(num_procs, multi.cpu_count()))
     print()
 
@@ -106,43 +115,44 @@ if __name__ == "__main__":
     manager = multi.Manager()
 
     # create shared versions of necessary variables TODO: del old refs?
-    #s_knots = multi.Array(ctypes.c_double, knots)
-    #s_idxs = multi.Array(ctypes.c_double, knot_idxs)
-    #s_parameters = multi.Array(ctypes.c_double, parameter_array.ravel())
-    #s_names = manager.list(struct_names)
-    #s_types = manager.list(types)
+    s_knots = multi.Array(ctypes.c_double, knots)
+    s_idxs = multi.Array(ctypes.c_double, knot_idxs)
+    s_parameters = multi.Array(ctypes.c_double, parameter_array.ravel())
+    s_names = manager.list(struct_names)
+    s_types = manager.list(types)
 
-    #del knots
-    #del knot_idxs
-    #del parameter_array
-    #del struct_names
-    #del types
+    s_workers = manager.list(workers)
 
-    # build evaluators
-
-    initialized_structures = manager.list()
-
-    def build_worker(name):
-        atoms = src.lammpsTools.atoms_from_file(name, types)
-
-        return Worker(atoms, knots, knot_idxs, types)
+    del knots
+    del knot_idxs
+    del parameter_array
+    del struct_names
+    del types
+    del workers
 
     def evaluate_structs(evaluator):
-        return evaluator.compute_energy(parameter_array)
+        #evaluator, parameters = args
 
+        return evaluator.compute_energy(s_parameters)
+
+    # start processor pool and begin distributing work
     pool = multi.Pool(num_procs)
 
-    initialized_evaluators = pool.map(build_worker, struct_names)
-    s_evaluators = manager.list(initialized_evaluators)
+    #initialized_evaluators = pool.map(build_worker, s_names
+    #        args=(s_knots, s_idxs, s_parameters, s_names, s_types)
+    #        )
 
-    del initialized_evaluators
+    #parameter_array = np.random.random(np.array(s_knots).shape[0] + 2*num_splines)
+    #parameter_array = np.atleast_2d(parameter_array)
+    #print(parameter_array[0,:5])
 
-    for i in range(3):
-        computed_energies = pool.map(evaluate_structs, s_evaluators)
-        print(np.concatenate(computed_energies))
+    #s_parameters = multi.Array(ctypes.c_double, parameter_array.ravel())
+    #del parameter_array
 
-        parameter_array = np.random.random(knots.shape[0] + 2*num_splines)
-        parameter_array = np.atleast_2d(parameter_array)
+    computed_energies = pool.map(evaluate_structs, s_workers)
+
+    print("Computed energies:\n{}".format(np.concatenate(computed_energies)))
+    print()
 
     print()
     print("Total runtime: {0}".format(time.time() - start))
