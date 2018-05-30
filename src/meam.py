5-30: (semi) functioning ga; fixing bug in ni scaling

src/meam.py
@@ -140,6 +140,7 @@ class MEAM:
             # Based on how fxns in MEAM are defined
             nsplines = ntypes * (ntypes + 4)
 
+            nphi = int((ntypes+1)*ntypes/2)
             # Build all splines; separate into different types later
             splines = []
             for i in range(nsplines):
@@ -157,12 +158,13 @@ class MEAM:
                     xcoords.append(x)
                     ycoords.append(y)
 
+                bc_type = ((1, d0), (1, dN))
+
                 # TODO: think this is wrong; TiO.meam.splines is an anomaly
                 # if (i < nphi+ntypes) or ((i >= nphi+2*ntypes) and (
                 #         i<nphi+3*ntypes)):
-                #     bc_type = ((), (1,0))
+                #     bc_type = ((2,0), (2,0))
 
-                bc_type = ((1, d0), (1, dN))
                 splines.append(Spline(xcoords, ycoords, bc_type=bc_type,
                                       end_derivs=(d0, dN)))
 
@@ -246,6 +248,8 @@ class MEAM:
 
                     phi = self.phis[ij_to_potl(itype, jtype, self.ntypes)]
 
+                    phi_idx = ij_to_potl(itype, jtype, self.ntypes)
+                    # logging.info("MEAM: phi_idx = {}".format(phi_idx))
                     # logging.info("MEAM: phi({0}) = {1}".format(r_ij, phi(r_ij)))
                     total_phi += phi(r_ij)
                 # end phi loop
@@ -878,12 +882,10 @@ def splines_to_pvec(splines):
     for s in splines:
         x_pvec = np.append(x_pvec, s.x)
 
-        der = []
         for i in range(len(s.x)):
             y_pvec = np.append(y_pvec, s(s.x[i]))
-            der.append(s(s.x[i], 1))
 
-        bc = [der[0], der[-1]]
+        bc = [s.d0, s.dN]
 
         y_pvec = np.append(y_pvec, bc)
 

src/worker.py
@@ -134,6 +134,8 @@ class Worker:
                 # Add distance/index/direction information to necessary lists
                 phi_idx = src.meam.ij_to_potl(itype, jtype, self.ntypes)
 
+                # logging.info("WORKER: phi_idx = {}".format(phi_idx))
+                # logging.info("WORKER: rij = {}".format(rij))
                 # phi
                 self.phis[phi_idx].add_to_energy_struct_vec(rij)
 
@@ -383,6 +385,7 @@ class Worker:
         for y, phi in zip(phi_pvecs, self.phis, ):
             if phi.energy_struct_vec.shape[0] > 0:
                 energy += phi.calc_energy(y)
+                # logging.info("WORKER: phi  = {}".format(phi.calc_energy(y)))
 
         # Embedding terms
         energy += self.embedding_energy(self.compute_ni(rho_pvecs, f_pvecs,\
@@ -408,7 +411,6 @@ class Worker:
         # Rho contribution
         for y, rho in zip(rho_pvecs, self.rhos):
             ni += rho.calc_energy(y).T
-            np.set_printoptions(precision=16)
             # logging.info("WORKER: rho =\n{0}".format(rho.calc_energy(y)))
 
         # Three-body contribution
@@ -434,6 +436,10 @@ class Worker:
             u_energy: total embedding energy
         """
 
+        # rescale into [0,1]
+        # TODO: this is a workaround; ALL of USpline needs to consider [0,1]
+        # ni = (ni - np.min(ni)) / (np.max(ni) - np.min(ni))
+
         for i, u in enumerate(self.us):
             u.energy_struct_vec = np.zeros((self.n_pots, 2*u.x.shape[0]+4))
             u.add_to_energy_struct_vec(ni[:, self.type_of_each_atom - 1 == i])
@@ -443,6 +449,10 @@ class Worker:
         for y, u in zip(u_pvecs, self.us):
 
             if len(u.energy_struct_vec) > 0:
+                # print("compute_zero_energy:", u.compute_zero_potential(y).ravel(), flush=True)
+                # print("calc_energy:", u.calc_energy(y).ravel(), flush=True)
+                # print("{} -- {}".format(y.shape[0], u.lhs_extrap_dist))
+                # print("{} -- {}".format(y.shape[0], u.rhs_extrap_dist))
                 u_energy -= u.compute_zero_potential(y).ravel()
                 u_energy += u.calc_energy(y)
                 # logging.info("WORKER: U = {0}".format(u.calc_energy(y)))
@@ -513,6 +523,9 @@ class Worker:
                 forces += phi.calc_forces(y)
 
         ni = self.compute_ni(rho_pvecs, f_pvecs, g_pvecs)
+
+        # ni = (ni - np.min(ni)) / (np.max(ni) - np.min(ni))
+
         uprimes = self.evaluate_uprimes(ni, u_pvecs)
 
         # Electron density embedding (rho)

src/workerSplines.py
@@ -127,6 +127,9 @@ class WorkerSpline:
             ws.energy_struct_vec = np.array(spline_data['energy_struct_vec'])
             ws.forces_struct_vec = np.array(spline_data['forces_struct_vec'])
 
+            # ws.energy_struct_vec = lil_matrix(ws.energy_struct_vec).tocsr()
+            # ws.forces_struct_vec = lil_matrix(ws.forces_struct_vec).tocsr()
+
         ws.n_knots = len(x)
 
         return ws
@@ -239,7 +242,8 @@ class WorkerSpline:
         nknots = knots.shape[0]
 
         # Perform interval search and prepare prefactors
-        all_k = np.digitize(x, knots, right=True) - 1
+        # all_k = np.digitize(x, knots, right=True) - 1
+        all_k = np.digitize(x, knots) - 1
         all_k = np.clip(all_k, 0, len(knots) - 2)
 
         prefactors = knots[all_k + 1] - knots[all_k]
@@ -432,6 +436,7 @@ class USpline(WorkerSpline):
         self.y = y
 
         z = np.zeros((self.y.shape[0], 2*self.y.shape[1]+4))
+
         z[:, 0] = self.y[:, 0] - self.y1[:, 0]*self.lhs_extrap_dist
         z[:, 1:1+self.n_knots] = self.y
         z[:, 1+self.n_knots] = self.y[:,-1] + self.y1[:,-1]*self.rhs_extrap_dist